c199d69f7d73d8a34a45cf64a36d630e4513a565
hadesfufu <yohan.masson@neuf.fr>
2017-06-08 11:22:38 +0200

    sweg 1

--- server.R
+++ server.R
@@ -1,26 +1,29 @@
 library(shiny)
 
-shinyServer(function(input,output,session){
-  
-}
-)
-
-
 function(input, output) {
   output$contents <- renderTable({
+      inFile <- input$file1
+      if (is.null(inFile))
+        return(NULL)
     
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
+      filedata <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
              quote=input$quote)
+      firstRow <- as.numeric(filedata[1,])
+      lastRow <- as.numeric(filedata[2,])
+      if(input$func == 'varcomp'){
+        varfr <- var(firstRow)
+        varlr <- var(lastRow)
+        outputdata <- data.frame(c(varfr), c(varlr))
+      }
+      else if(input$func == 'moycomp'){
+        moyfr <- mean(firstRow)
+        moylr <- mean(lastRow)
+        outputdata <- data.frame(c(moyfr), c(moylr))
+      }
+      else{
+        outputdata <- firstRow 
+      }
+      outputdata
   })
 }
+
--- ui.R
+++ ui.R
@@ -1,32 +1,33 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-library(shinyjs)
 
-shinyUI(pageWithSidebar(
-  headerPanel("Title"),
-  sidebarPanel( 
-    fileInput('file1', 'Choose CSV File',accept=
-                c('text/csv', 
-                'text/comma-separated-values,text/plain', 
-                '.csv')),
-    tags$hr(),
-    checkboxInput('header', 'Header', TRUE),
-    radioButtons('sep', 'Separator',
-                 c(Comma=',',
-                   Semicolon=';',
-                   Tab='\t'),
-                 ','),
-    radioButtons('quote', 'Quote',
-                 c(None='',
-                   'Double Quote'='"',
-                   'Single Quote'="'"),
-                 '"')
-  ),
-  mainPanel()
-))
+fluidPage(
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose CSV File',
+                accept=c('text/csv', 
+                         'text/comma-separated-values,text/plain', 
+                         '.csv')),
+      tags$hr(),
+      checkboxInput('header', 'Header', FALSE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ';'),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   ''),
+      radioButtons('func', 'Function',
+                   c(None='',
+                     'Comparaison de Variance'='varcomp',
+                     'Comparaison de Moyenne'="moycomp"))
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
+)
