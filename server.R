b4cb796361ce0f3373ed6d6b459539c329471dc0
ThomasGille <totogille@gmail.com>
2017-06-08 17:14:22 +0200

    created tabs

--- server.R
+++ server.R
@@ -6,22 +6,23 @@ function(input, output) {
       if (is.null(inFile))
         return(NULL)
     
-      filedata <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
+      filedata <- read.csv(inFile$datapath, header=input$header, sep=input$sep,
              quote=input$quote)
-      firstRow <- as.numeric(filedata[1,])
-      lastRow <- as.numeric(filedata[2,])
+      firstCol <- as.numeric(filedata[,1])
+      lastCol <- as.numeric(filedata[,2])
       if(input$func == 'varcomp'){
-        varfr <- var(firstRow)
-        varlr <- var(lastRow)
-        outputdata <- data.frame(c(varfr), c(varlr))
+        varfc <- var(firstCol)
+        varlc <- var(lastCol)
+        outputdata <- data.frame(c(varfc), c(varlc))
       }
       else if(input$func == 'moycomp'){
-        moyfr <- mean(firstRow)
-        moylr <- mean(lastRow)
-        outputdata <- data.frame(c(moyfr), c(moylr))
+        moyfc <- mean(firstCol)
+        moylc <- mean(lastCol)
+        outputdata <- t.test(firstCol, lastCol)
+        outputdata <- data.frame(c(moyfc), c(moylc))
       }
       else{
-        outputdata <- firstRow 
+        outputdata <- filedata 
       }
       outputdata
   })
